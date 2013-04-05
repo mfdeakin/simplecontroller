8b91fb1ec89be3ee42a2212aed837ead6d4e548c
mfdeakin <mdever44@gmail.com>
2013-04-05 19:55:04 -0400

    Motor controller cleanups and comments

--- motor.cpp
+++ motor.cpp
@@ -1,17 +1,55 @@
 
 #include "motor.h"
 
+/* Structure used to keep up with the state of the motor controller */
 struct motorctrl
 {
+  /* The serial port the motor controller is connected to */
   USARTClass *serial;
+  /* Whether or not the motor controller was detected */
   bool attached;
 };
 
+/* Checks whether the motor controller is attached
+ * Preconditions: A valid motor controller, a positive timeout value
+ * Postconditions: An up to date check for the motor controller,
+ * 		   completed in no more than timeout ms
+ */
 bool motorCheckAttached(struct motorctrl *motor, int timeout);
 
+/* Reads a byte from the motor controllers serial port.
+ * Needed because the Arduino library doesn't allow the program
+ * to specify the correct data format in serial initialization
+ * Preconditions: A valid serial object, connected to the motor controller
+ * Postconditions: A character sent by the motor controller
+ */
 char motorReadByte(USARTClass *serial);
+
+/* Writes a string to the motor controllers serial port.
+ * Needed because the Arduino library doesn't allow the program
+ * to specify the correct data format in serial initialization
+ * Preconditions: A valid serial port, a non null pointer to
+ * 		  a zero terminated string
+ * Postconditions: The string was written to the motor controller
+ */
 void motorWriteString(USARTClass *serial, const char *str);
+
+/* Writes a byte to the motor controllers serial port.
+ * Needed because the Arduino library doesn't allow the program
+ * to specify the correct data format in serial initialization
+ * Preconditions: A valid serial port, a byte to be sent
+ * Postconditions: The byte was written to the motor controller in
+ * 		   the valid format
+ */
 void motorWriteByte(USARTClass *serial, byte b);
+
+/* Writes a set of bytes to the motor controllers serial port.
+ * Needed because the Arduino library doesn't allow the program
+ * to specify the correct data format in serial initialization
+ * Preconditions: A valid serial port, a pointer to the bytes to be sent
+ * Postconditions: The byte was written to the motor controller in
+ * 		   the valid format
+ */
 void motorWriteBytes(USARTClass *serial, const void *bytes, size_t len);
 
 struct motorctrl *motorInit(USARTClass *serial, int timeout)
@@ -19,26 +57,32 @@ struct motorctrl *motorInit(USARTClass *serial, int timeout)
   struct motorctrl *motor = (struct motorctrl *)malloc(sizeof(struct motorctrl));
   if(!motor)
     return NULL;
+  memset(motor, 0, sizeof(struct motorctrl));
+  /* The motor controller communicates at 9600 baud */
   motor->serial = serial;
   motor->serial->begin(9600);
   if(!motorCheckAttached(motor, timeout)) {
     free(motor);
     return NULL;
   }
-  /* Set the motor controller to mixed mode.
-   * Before we do that, read the current mode in so we don't change things unnecessarily
-   */
   return motor;
 }
 
 void motorFree(struct motorctrl *motor)
 {
+  /* Turn off the motors, then free the memory */
+  motorSetSpeed(motor, 0, 0);
   free(motor);
 }
 
 struct channelpair motorCheckAmp(struct motorctrl *motor)
 {
-  char buffer[6];
+  /* Motor Response Format:
+   * ##\n##\n
+   * Where the numbers are in hex and range from 0 to 7F
+   * \n is a newline
+   */
+  char buffer[7];
   if(!motorWriteCmd(motor, "?a", buffer, sizeof(char[6]), 1000)) {
     DEBUGPRINT("Could not read amps!\r\n");
     return {0, 0};
@@ -49,6 +93,7 @@ struct channelpair motorCheckAmp(struct motorctrl *motor)
   DEBUGSERIAL.print("Values read:\r\n");
   DEBUGSERIAL.print(buffer);
   DEBUGSERIAL.print("\r\n");
+  /* Convert the values to integers */
   int check = sscanf(buffer, "%xd\n%xd\n", &values.cA, &values.cB);
   DEBUGSERIAL.print("Read amp values: ");
   DEBUGSERIAL.print(check);
@@ -62,6 +107,11 @@ struct channelpair motorCheckAmp(struct motorctrl *motor)
 
 struct channelpair motorCheckVolt(struct motorctrl *motor)
 {
+  /* Motor Response Format:
+   * ## ##\r
+   * Where the numbers are in hex and range from 0 to 7F
+   * \r is a carriage return
+   */
   char buffer[7];
   if(!motorWriteCmd(motor, "?v", buffer, sizeof(char[6]), 1000)) {
     DEBUGPRINT("Could not read voltages!\r\n");
@@ -73,6 +123,7 @@ struct channelpair motorCheckVolt(struct motorctrl *motor)
   DEBUGSERIAL.print("Values read:\r\n");
   DEBUGSERIAL.print(buffer);
   DEBUGSERIAL.print("\r\n");
+  /* Convert the values to integers */
   int check = sscanf(buffer, "%xd\n%xd\n", &values.cA, &values.cB);
   DEBUGSERIAL.print("Read volt values: ");
   DEBUGSERIAL.print(check);
@@ -84,16 +135,21 @@ struct channelpair motorCheckVolt(struct motorctrl *motor)
   return values;
 }
 
-bool motorWriteCmd(struct motorctrl *motor, const char *cmd,
-		   void *buffer, size_t size, int timeout)
+int motorWriteCmd(struct motorctrl *motor, const char *cmd,
+		  void *buffer, size_t size, int timeout)
 {
   /* Clear out the serial buffer, we don't want to worry about overflow */
   while(motor->serial->available())
     motor->serial->read();
-  DEBUGPRINT(cmd);
+  /* Write the comand to the motor controller, followed by an EOL so the
+   * controller knows it's recieved an entire command 
+   */
   motorWriteString(motor->serial, cmd);
   motorWriteString(motor->serial, "\r\n");
   motor->serial->flush();
+  /* Read the output of the motor controller, but take no longer than
+   * the timeout specified
+   */
   motor->serial->setTimeout(timeout);
   if(timeout > 0) {
     if(timeout > 10)
@@ -104,6 +160,10 @@ bool motorWriteCmd(struct motorctrl *motor, const char *cmd,
   unsigned starttime = GetTickCount(),
     delta = GetTickCount() - starttime,
     i;
+  /* The motor controller should echo the command back,
+   * so find that to know that we're reading what we're looking for.
+   * Typical state machine logic
+   */
   for(i = 0;
       cmd[i] && delta * TICKSPERTENMS < timeout;
       delta = GetTickCount() - starttime) {
@@ -125,7 +185,11 @@ bool motorWriteCmd(struct motorctrl *motor, const char *cmd,
     }
   }
   if(cmd[i]) {
-    return false;
+    /* We couldn't find the command in the echo with the time provided,
+     * so give up. (The number of characters read which matched the command
+     * was less than the number of characters in the command
+     */
+    return -1;
   }
   byte *buf = (byte *)buffer;
   for(i = 0;
@@ -133,17 +197,23 @@ bool motorWriteCmd(struct motorctrl *motor, const char *cmd,
       i++, delta = GetTickCount() - starttime) {
     buf[i] = motorReadByte(motor->serial);
   }
-  if(i < size)
-  return true;
+  return i;
 }
 
 void motorSetSpeed(struct motorctrl *motor, float fwd, float rot)
 {
+  /* This is a simple command which doesn't require a response
+   * from the motor controller, so just build it and run it.
+   */
   int forward = fwd * 0x7F,
     rotation = rot * 0x7F;
   char cmdA = 'A',
     cmdB = 'B';
+  /* Set channel A */
   if(forward < 0) {
+    /* All values should be positive, the motor controller determines
+     * sign based on the case of the command character
+     */
     forward = -forward;
     cmdA = 'a';
   }
@@ -151,6 +221,7 @@ void motorSetSpeed(struct motorctrl *motor, float fwd, float rot)
   sprintf(buffer, "!%c%02X\r\n", cmdA, forward);
   DEBUGPRINT(buffer);
   motorWriteString(motor->serial, buffer);
+  /* Set channel B */
   if(rotation < 0) {
     rotation = -rotation;
     cmdB = 'b';
@@ -163,6 +234,12 @@ void motorSetSpeed(struct motorctrl *motor, float fwd, float rot)
 
 bool motorCheckAttached(struct motorctrl *motor, int timeout)
 {
+  /* Check for the motor controller
+   * This is a little iffy, but the motor controller consistently responds
+   * with OK if an indeterminant number of carriage returns are sent to it.
+   * The number is not 10, which one might suspect given the documentation
+   * on how to bring the motor controller into serial command.
+   */
   const char *states = "OK";
   const int statelen = 2;
   /* The motor needs to be sent 10 carriage returns before it
@@ -176,7 +253,7 @@ bool motorCheckAttached(struct motorctrl *motor, int timeout)
     DEBUGSERIAL.print("Checking for motor controller connection\r\n");
     for(int i = 0; i < 20; i++)
       motorWriteByte(motor->serial, '\r');
-    /* Give the motor controller a chance to respond, wait 100 ms */
+    /* Give the motor controller a chance to respond, wait 50 ms */
     delay(50);
     timeout -= 50;
     if(motor->serial->available() > 0) {
@@ -218,11 +295,16 @@ bool motorCheckAttached(struct motorctrl *motor, int timeout)
  */
 char motorReadByte(USARTClass *serial)
 {
+  /* Just strip off the parity bit */
   return serial->read() & 0x7f;
 }
 
 void motorWriteString(USARTClass *serial, const char *str)
 {
+  /* Just iterate over the strings bytes, and send those.
+   * Don't send the null terminator, though. Let the user do that
+   * in the odd instance that they need to.
+   */
   for(int i = 0; str[i]; i++) {
     motorWriteByte(serial, str[i]);
   }
@@ -230,6 +312,9 @@ void motorWriteString(USARTClass *serial, const char *str)
 
 void motorWriteBytes(USARTClass *serial, const void *bytes, size_t len)
 {
+  /* Write the bytes to the motor controller by iterating over them
+   * and sending them individually.
+   */
   byte *b = (byte *)bytes;
   for(int i = 0; i < len; i++) {
     motorWriteByte(serial, b[i]);
@@ -238,6 +323,12 @@ void motorWriteBytes(USARTClass *serial, const void *bytes, size_t len)
 
 void motorWriteByte(USARTClass *serial, byte b)
 {
+  /* The motor controller expects the following format:
+   * pxxx xxxx
+   * Where p is the parity bit.
+   * To build this, remove the 7th bit, the determine the parity of the
+   * remaining bits. Finally, add in the parity bit.
+   */
   b <<= 1;
   byte parity = 0;
   byte temp = b;
--- motor.h
+++ motor.h
@@ -9,19 +9,69 @@
 
 struct motorctrl;
 
+/* Structure corresponding to the motor controllers channels
+ * cA -> Channel A
+ * cB -> Channel B
+ */
 struct channelpair {
 	int cA, cB;
 };
 
+/* Initializes the motor controller object connected to the serial port
+ * If no motor controller is detected in the required timeout, returns NULL.
+ * Preconditions: A valid serial port, a positive timeout
+ * Postconditions: The motor controller is detected within the timeout and
+ *                 then initialized.
+ */
 struct motorctrl *motorInit(USARTClass *serial, int timeout);
-/* Frees the motor controller, turns off the motors */
-void motorFree(struct modem *);
 
-bool motorWriteCmd(struct motorctrl *, const char *cmd,
+/* Frees the motor controller, turns off the motors
+ * Preconditions: A valid motor controller object
+ * Postconditions: The motor controller object memory is released,
+ *                 The motor controllers motors are turned off
+ */
+void motorFree(struct motorctrl *);
+
+/* Writes a command to the motor controller, and waits for a response.
+ * Returns the number of bytes sent in response to the command
+ * Preconditions: A valid motor controller object, a valid null terminated
+ *                command string, a valid buffer of size size,
+ *								and a positive timeout.
+ * Postconditions: The command is written to the motor controller, and the
+ *                 response of the motor controller is returned within the.
+ *                 timeout specified.
+ *                 Any information buffered before the command is sent is
+ *                 discarded. Any information that is sent after the specified
+ *                 amount of data is recieved is also discarded.
+ */
+int motorWriteCmd(struct motorctrl *, const char *cmd,
 									 void *buffer, size_t size, int timeout);
 
+/* Sets the speeds of the motors
+ * Uses a differential controller
+ * Preconditions: A valid motor object, floating point values between -1 and 1
+ *								1 is full power forward, 0 is off, -1 is full power reverse
+ * Postconditions: The motor controller powers the motors at the percent
+ *                 specified.
+ */
 void motorSetSpeed(struct motorctrl *, float forward, float rotate);
+
+/* Reads the number of amps that the motor controller is providing
+ * to the motors. Returns in a channel pair struct, corresponding to
+ * each motor.
+ * Preconditons: A valid motor object.
+ * Postconditions: The state of the motor controller is preserved,
+ *                 The number of amps being sent to each motor is updated
+ */
 struct channelpair motorCheckAmp(struct motorctrl *);
-struct channelpair motorCheckVolt(struct motorctrl *);
+
+/* Reads the average number of watts that the motor controller is providing
+ * to the motors. Returns in a channel pair struct, corresponding to
+ * each motor.
+ * Preconditons: A valid motor object.
+ * Postconditions: The state of the motor controller is preserved,
+ *                 The number of watts being sent to each motor is updated
+ */
+struct channelpair motorCheckWatt(struct motorctrl *);
 
 #endif
